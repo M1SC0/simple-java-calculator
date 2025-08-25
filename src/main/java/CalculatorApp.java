ffb5e874f46407af5294bdf9c96c258fe71e9bda
Riccardo <riccardo.finocchiaro5@gmail.com>
2025-08-25 19:59:13 +0200

    feat: created GUI

---
+++ src/main/java/CalculatorApp.java
@@ -0,0 +1,9 @@
+import view.CalculatorGui;
+
+public class CalculatorApp {
+    public static void main(String[] args) {
+        CalculatorGui calculator = new CalculatorGui();
+        calculator.open();
+    }
+
+}
---
+++ src/main/java/view/CalculatorGui.java
@@ -0,0 +1,101 @@
+package view;
+
+import org.example.util.CommonCostants;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CalculatorGui extends JFrame implements ActionListener {
+    private final SpringLayout springLayout = new SpringLayout();
+
+    // display field
+    private JTextField displayField;
+
+    // buttons
+    private JButton[] buttons;
+
+    // flags
+    private boolean pressedOperator = false;
+    private boolean pressedEquals = false;
+
+    public CalculatorGui() {
+        super(CommonCostants.APP_NAME);
+        setSize(CommonCostants.APP_SIZE[0], CommonCostants.APP_SIZE[1]);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setLayout(springLayout);
+
+        addGuiComponents();
+    }
+
+    private void addGuiComponents() {
+        // add display components
+        addDisplayFieldComponent();
+
+        // add buttons components
+        addButtonComponents();
+    }
+
+    private void addDisplayFieldComponent() {
+        JPanel displayFieldPanel = new JPanel(new BorderLayout());
+        displayField = new JTextField(CommonCostants.TEXTFIELD_LENGHT);
+        displayField.setFont(new Font("Dialog", Font.BOLD, CommonCostants.TEXTFIELD_FONTSIZE));
+        displayField.setEditable(false);
+        displayField.setText("0");
+        displayField.setHorizontalAlignment(SwingConstants.RIGHT);
+
+        displayFieldPanel.add(displayField, BorderLayout.CENTER);
+
+        this.getContentPane().add(displayFieldPanel);
+        springLayout.putConstraint(SpringLayout.NORTH, displayFieldPanel, CommonCostants.TEXTFIELD_SPRINGLAYOUT_NORTHPAD, SpringLayout.NORTH, this);
+        springLayout.putConstraint(SpringLayout.WEST, displayFieldPanel, CommonCostants.TEXTFIELD_SPRINGLAYOUT_WESTPAD, SpringLayout.WEST, this);
+    }
+
+    private void addButtonComponents() {
+        GridLayout gridLayout = new GridLayout(CommonCostants.BUTTON_ROWCOUNT, CommonCostants.BUTTON_COLCOUNT);
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(gridLayout);
+        buttons = new JButton[CommonCostants.BUTTON_COUNT];
+        for (int i = 0; i < CommonCostants.BUTTON_COUNT; i++) {
+            JButton button = new JButton(getButtonLabel(i));
+            button.setFont(new Font("Dialog", Font.PLAIN, CommonCostants.BUTTON_FONTSIZE));
+            button.addActionListener(this);
+
+            buttonPanel.add(button);
+        }
+
+        gridLayout.setHgap(CommonCostants.BUTTON_HGAP);
+        gridLayout.setVgap(CommonCostants.BUTTON_VGAP);
+
+        this.getContentPane().add(buttonPanel);
+        springLayout.putConstraint(SpringLayout.NORTH, buttonPanel, CommonCostants.BUTTON_SPRINGLAYOUT_NORTHPAD, SpringLayout.NORTH, this);
+        springLayout.putConstraint(SpringLayout.WEST, buttonPanel, CommonCostants.BUTTON_SPRINGLAYOUT_WESTPAD, SpringLayout.WEST, this);
+
+
+    }
+
+    private String getButtonLabel(int buttonIndex) {
+        switch (buttonIndex) {
+            case 0: return "7";  case 1: return "8";  case 2: return "9";  case 3: return "/";
+            case 4: return "4";  case 5: return "5";  case 6: return "6";  case 7: return "x";
+            case 8: return "1";  case 9: return "2";  case 10: return "3"; case 11: return "-";
+            case 12: return "."; case 13: return "0"; case 14: return "+"; case 15: return "=";
+
+            default: return "";
+        }
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String buttonCommand = e.getActionCommand();
+        // TODO: implement calculator logic here, 3:43:47
+    }
+
+    public void open() {
+        setVisible(true);
+    }
+}
